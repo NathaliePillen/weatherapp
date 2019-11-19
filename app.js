5ddd2cf97167c4098de86299928748644426eeb8
nathalie <pillennathalie@gmail.com>
2019-11-19 09:17:54 +0100

    fetch

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 window.addEventListener('load', () => {
     let long;
-    let lat;
+    let latitude;
     let temparatureDescription = document.querySelector('.temperature-discription');
-    let temeratureDegree = document.querySelector('temperature-degree');
+    let temperatureDegree = document.querySelector('temperature-degree');
     let locationTimezone = document.querySelector('temperature-timezone');
+
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -18,9 +19,24 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     console.log(data);
-                    const { weather } = data.list;
+                    console.log(data.list.length);
+
+                    for (i = 0; i < data.list.length; i++) {
+                        console.log(i);
+                        if (i === 0) {
+                            let first = data.list[i].main.temp;
+                            console.log(first);
+                            document.querySelector(".temperature.degree-section.temperature-degree").innerHTML = "first";
+                        }
+                        if (i === 8) {
+                            let second = data.list[i].main.temp;
+                            console.log(second);
+                        }
+                    }
 
                 });
         });
     }
+
+
 });
